5ba5d4e6fd2679b53b20d4648af4f87758a578f7
zwade80-hue <zwade80@gmail.com>
2026-05-13 13:33:47 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,20 @@
-const CACHE = "dutch-vocab-v1";
+const CACHE = "dutch-vocab-v3";
 const FILES = ["/", "/index.html", "/words.json", "/manifest.json", "/benelux.png", "/icon.svg"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
